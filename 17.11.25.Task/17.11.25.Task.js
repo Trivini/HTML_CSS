7a75efa6b098a2dcddbc02ba705de768510017e8
Trivini <sakshis8767@gmail.com>
2025-12-04 19:24:03 +0530

    variables and datatype

---
+++ 17.11.25.Task/17.11.25.Task.js
@@ -0,0 +1,20 @@
+
+        var result = [
+            {
+               name : "Apple iPhone 15 (128 GB)-Black",
+               status : "5K+ bought in past month",
+               price : "₹51,990 M.R.P: ₹59,900M.R.P: ₹59,900 (13% off)",
+               delivery : "FREE delivery Sun, 23 Nov",
+               cart : "add to cart"
+            },
+            {
+                name: "iPhone 16 128 GB: 5G Mobile Phone",
+                status: "500+ bought in past month",
+                price: "₹66,900 M.R.P: ₹79,900 (16% off)",
+                //delivery: "FREE delivery Tue, 25 Nov",
+                //cart: "Add to cart"
+            },
+            
+        
+        ];
+    
